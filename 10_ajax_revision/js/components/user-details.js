7d7e616c4923f90768916258734f1b81319f01aa
Christian Lisangola <jochri3@gmail.com>
2022-08-04 12:25:44 +0300

    add users details component in page

---
+++ 10_ajax_revision/js/components/user-details.js
@@ -0,0 +1,9 @@
+export function UserDetails(user) {
+  return `<h1>${user.name}</h1>
+     <p>${user.email}</p><p>${user.phone}</p>`;
+}
+
+// export function UserDetails({ name, email, phone }) {
+//   return `<h1>${name}</h1>
+//      <p>${email}</p><p>${phone}</p>`;
+// }
--- 10_ajax_revision/js/details.js
+++ 10_ajax_revision/js/details.js
@@ -1,9 +1,13 @@
+import { UserDetails } from "./components/user-details.js";
 import { BASE_URL } from "./constants.js";
 
 const urlParams = new URLSearchParams(location.search);
-
 const id = urlParams.get("id");
 
+const userDetails = document.querySelector("#user-details");
+
 axios.get(`${BASE_URL}/${id}`).then(function (response) {
-  console.log(response.data);
+  console.log(response);
+  const user = response.data;
+  userDetails.innerHTML = UserDetails(user);
 });
