1aa4362b3145061d0efe572b484cb99679ee4ebf
Christian Lisangola <jochri3@gmail.com>
2022-08-04 12:09:14 +0300

    add global constants

--- 10_ajax_revision/js/details.js
+++ 10_ajax_revision/js/details.js
@@ -1,9 +1,9 @@
+import { BASE_URL } from "./constants.js";
+
 const urlParams = new URLSearchParams(location.search);
 
 const id = urlParams.get("id");
 
-axios
-  .get(`https://jsonplaceholder.typicode.com/users/${id}`)
-  .then(function (response) {
-    console.log(response.data);
-  });
+axios.get(`${BASE_URL}/${id}`).then(function (response) {
+  console.log(response.data);
+});
--- 10_ajax_revision/js/home.js
+++ 10_ajax_revision/js/home.js
@@ -1,13 +1,12 @@
 import { UserCard } from "./components/user-card.js";
+import { BASE_URL } from "./constants.js";
 
 const usersCards = document.querySelector("#users");
 
-axios
-  .get("https://jsonplaceholder.typicode.com/users")
-  .then(function (response) {
-    const users = response.data;
-    for (let user of users) {
-      const card = UserCard(user);
-      usersCards.appendChild(card);
-    }
-  });
+axios.get(BASE_URL).then(function (response) {
+  const users = response.data;
+  for (let user of users) {
+    const card = UserCard(user);
+    usersCards.appendChild(card);
+  }
+});
